c9391e34e8284191a78b1a84e2166d7009a9ce18
Merilin-Grete Kakko <merilingrete.kakko@gmail.com>
2022-08-26 14:32:08 +0100

    Changed OneCall version, 3.0 doesn't work

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 function getForecast(coordinates) {
   let apiKey = "749066f0dd440bc71c8cce63998ab3d2";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
 }
